0438ba296936e7a5b005acac54d14ebf9f8b6cd8
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-06-10 23:59:08 -0300

    Alterado configuração para maximizaçao da home

--- CRUD_SQLite.SemCamadas/Formulario/Home.Designer.cs
+++ CRUD_SQLite.SemCamadas/Formulario/Home.Designer.cs
@@ -155,6 +155,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home";
